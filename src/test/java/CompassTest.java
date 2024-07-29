4a29eeb0559dbb914a83bcf5476869145fd719c5
dMarsh <125702046+Alan-Turing13@users.noreply.github.com>
2024-07-29 11:56:42 +0100

    add assertAll

--- src/test/java/CompassTest.java
+++ src/test/java/CompassTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,7 +17,41 @@ class CompassTest {
     @DisplayName("rotates a given point according to a given direction")
     void testRotate() {
         Point currentP = compass.rotate(Point.NORTH, Direction.RIGHT);
-
         assertEquals(Point.EAST, currentP);
+    }
+
+    @Test
+    @DisplayName("rotating right")
+    void testRotateRight() {
+        assertAll(
+                new Executable() {
+                    @Override
+                    public void execute() throws Throwable {
+                        assertEquals(Point.EAST, compass.rotate(Point.NORTH, Direction.RIGHT));
+                    }
+                },
+                new Executable() {
+                    @Override
+                    public void execute() throws Throwable {
+                        assertEquals(Point.EAST, compass.rotate(Point.NORTH, Direction.RIGHT));
+                    }
+                },
+                new Executable() {
+                    @Override
+                    public void execute() throws Throwable {
+                        assertEquals(Point.WEST, compass.rotate(Point.SOUTH, Direction.RIGHT));
+                    }
+                },
+                new Executable() {
+                    @Override
+                    public void execute() throws Throwable {
+                        assertEquals(Point.NORTH, compass.rotate(Point.WEST, Direction.RIGHT));
+                    }
+                }
+        );
+
+    }
+
+    private void assertAll(Point rotate, Point rotate1, Point rotate2, Point rotate3) {
     }
 }
